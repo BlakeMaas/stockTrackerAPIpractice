769e6ef1530e8a5dffab7b951a95b32452e519bd
Blake Maas <126283986+BlakeMaas@users.noreply.github.com>
2025-04-07 21:03:34 -0700

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,9 @@
 # tests/test_utils.py
 from utils import fetch_daily_stock_data
+import os
+
+def test_secret_key():
+    assert os.environ.get("SECRET_KEY") == "42"
 
 def test_fetch_daily_stock_data_structure():
     data = fetch_daily_stock_data("AAPL")
