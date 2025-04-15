7f05c85514fbef5c69fd9eb7bedceef4edd52838
Blake Maas <126283986+BlakeMaas@users.noreply.github.com>
2025-04-14 21:48:28 -0700

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -2,8 +2,13 @@
 from utils import fetch_daily_stock_data
 import os
 
-def test_secret_key():
-    assert os.environ.get("SECRET_KEY") == "42"
+import os
+
+api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
+
+if not api_key:
+    raise RuntimeError("Missing ALPHA_VANTAGE_API_KEY environment variable")
+
 
 def test_fetch_daily_stock_data_structure():
     data = fetch_daily_stock_data("AAPL")
